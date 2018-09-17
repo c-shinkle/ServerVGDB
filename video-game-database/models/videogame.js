3567fc13fb2ae0a12a67f3d7164b21552f7584bf
c-shinkle <cshinkle@iastate.edu>
2018-09-17 15:01:23 -0500

    Added video game schema

---
+++ video-game-database/models/videogame.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+var VideoGameSchema = new Schema(
+  {
+    title: {type: String, required: true},
+    devloper: {type: String, required: true},
+    release_date: {type: Date, required: true},
+  }
+);
+
+module.export = mongoose.model('Vidoe Game', VideoGameSchema);
