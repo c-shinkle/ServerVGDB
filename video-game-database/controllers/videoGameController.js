afbe4bdd21a28f1d9ad1911705ae5e805acbb0d7
Christian <cshinkle@iastate.edu>
2018-09-21 13:30:41 -0500

    Added stubs for controller

---
+++ video-game-database/controllers/videoGameController.js
@@ -0,0 +1,41 @@
+var Video_Game = require('../models/videoGame');
+
+// Display list of all Video Games.
+exports.video_game_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Video Game list');
+};
+
+// Display detail page for a specific Video Game.
+exports.video_game_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Video Game detail: ' + req.params.id);
+};
+
+// Display Video Game create form on GET.
+exports.video_game_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Video Game create GET');
+};
+
+// Handle Video Game create on POST.
+exports.video_game_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Video Game create POST');
+};
+
+// Display Video Game delete form on GET.
+exports.video_game_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Video Game delete GET');
+};
+
+// Handle Video Game delete on POST.
+exports.video_game_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Video Game delete POST');
+};
+
+// Display Video Game update form on GET.
+exports.video_game_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Video Game update GET');
+};
+
+// Handle Video Game update on POST.
+exports.video_game_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Video Game update POST');
+};
